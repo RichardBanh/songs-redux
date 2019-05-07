90fa5a0df1c2672c7049aa871eb6cc445d09cc81
rbanh <rbanh@me.com>
2019-05-07 00:41:34 -0700

    redux boiler plate

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,12 +1,8 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
 class App extends Component {
- 
-  render() { 
-    return ( <div>
-      App
-    </div> );
+  render() {
+    return <div>App</div>;
   }
 }
- 
+
 export default App;
---
+++ src/Reducers/index.js
@@ -0,0 +1,23 @@
+import {combineReducers} from 'redux';
+
+const songsReducer = () => {
+  return [
+    {title: 'No Scrubs', duration:'4:05'},
+    {title:'Macarena', duration:'1:43'},
+    {title: 'All Star', duration: '3:15'},
+    {title: 'I want it that way', duration:'5:05'}
+  ];
+}
+
+const selectedSongReducer = (selectedSong=null, action) => {
+
+  if(action.type==='SONG_SELECTED') {
+    return action.payload;
+  }
+
+}
+
+export default combineReducers({
+  songs: songsReducer,
+  selectedSong: selectedSongReducer
+})
